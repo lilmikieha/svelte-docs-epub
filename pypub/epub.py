28c8bada204ac562d41bd68a6a7da5c3ae0a7a13
Ankur Seth <iankurseth@gmail.com>
2025-05-07 08:44:52 +0530

    Cleaned up html, got toc working

--- pypub/epub.py
+++ pypub/epub.py
@@ -34,14 +34,35 @@ class EpubChapter:
 	def get_title(self):
 		if not self.soup:
 			self.setup_soup()
-		return self.soup.title
+		return self.soup.title.decode_contents()
 
 	def get_content(self):
 		if not self.soup:
 			self.setup_soup()
 		docs_content = self.soup.find("div", id="docs-content")
+		for div in docs_content.find_all("div", class_="breadcrumbs"):
+			div.decompose() # removes breadcrumbs
+
+		for span in docs_content.find_all("span", class_="twoslash-popup-container"):
+			span.decompose() # removes code popups
+
+		for p in docs_content.find_all("p", class_="edit"):
+			p.decompose() # removes edit section
+
+		for div in docs_content.find_all("div", class_="controls"):
+			div.decompose() # removes next/previous links
+
+		for details_tag in docs_content.find_all('details'):
+			details_tag.name = 'div'
+
+		for summary_tag in docs_content.find_all('summary'):
+			summary_tag.name = 'h4'
+
 		for tag in docs_content.find_all(True):
-			tag.attrs = {}
+			if tag.name in ['input', 'button']:
+				tag.decompose()
+			else:
+				tag.attrs = {}
 		return str(docs_content)
 
 
